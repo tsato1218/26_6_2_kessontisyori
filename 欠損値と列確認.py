a65c91459443a7291bc84fd96172ef99b536c2be
tsato1218 <tsato5796@gmail.com>
2026-06-28 02:04:23 +0900

    Update 欠損値と列確認.py

--- 欠損値と列確認.py
+++ 欠損値と列確認.py
@@ -1,29 +1,35 @@
 import pandas as pd
 
-#これは確認するだけのコード列多いと使えない
-
-file_path=r".csv"#パスを入れるとこ
-df=pd.read_csv(file_path)#dfを作成
+file_path = r"C:\Users\81805\Documents\train.csv"
+df = pd.read_csv(file_path)
 
 def NA_rate(dataframe):
-    print('==========df.info=============================')#確認
-    df.info()
-    col_count = df.shape[1]
-    
+    print('========== dataframe.info =============================')
+    dataframe.info()
+
+    print('\n========== 行数・列数 =============================')
+    print(dataframe.shape)
+
+    col_count = dataframe.shape[1]
+
     if col_count >= 20:
-        print("!!!!!!!!!!!!!!!!!!!!!!!列が多いかもしれない!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(f"警告: 列数が{col_count}列あります。列が多い")
     
-    print('==========欠損値の割合======================')#10%付近超えてたら見に行く
-    missing_rate = df.isna().mean() * 100
+    print('\n========== 欠損値の割合 10%以上 ======================')
+    missing_rate = dataframe.isna().mean() * 100
     print(missing_rate[missing_rate >= 10].sort_values(ascending=False))
+
+    print('\n========== カテゴリ列の確認 ======================')
+    cat_cols = dataframe.select_dtypes(include=["object", "category", "string"]).columns
     
-    for i in df.columns:
-        if df[i].dtype== object:#もっと簡単な奴があるらしいぞ.select_dtypes(include=["object", "category", "string"]).columns
-            print(f'=============={i}の中身の数=================')
-            print(df[i].nunique())#名前とかIDとか多くなるから確認
+    for col in cat_cols:
+        unique_count = dataframe[col].nunique(dropna=False)
+
+        print(f'\n============== {col} の確認 =================')
+        print(f'種類数: {unique_count}')
     
-            if df[i].nunique()<=20:
-                print(f'==========={i}の中身==========')
-                print(df[i].value_counts())#20くらいだと違う表現してるだけかもしれん
+        if unique_count <= 20:
+            print(f'=========== {col} の中身 ===========')
+            print(dataframe[col].value_counts(dropna=False))
 
 NA_rate(df)
